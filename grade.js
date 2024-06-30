52c50571b22f0c5919119ec01549049f3c482be7
Imelda Hope <imeldalarusso@gmail.com>
2024-06-30 17:53:16 +0300

    creates function for grading system

---
+++ grade.js
@@ -0,0 +1,27 @@
+//Generate student grade based on the grading scale provided.
+// A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+
+function studentGrades(score = 67){
+    let grade;
+    switch (true) {        
+        case score > 70:
+            grade = 'A';
+            break;
+        case score >=60 && score <= 79:
+            grade = 'B';
+            break;
+        case score >= 49 && score <= 59:
+            grade = 'C';
+            break;
+        case score >= 40 && score < 49:
+            grade = 'D';
+            break;    
+        default:
+            grade = 'E';
+            break;
+    }
+    //console.log(`The student\'s grade is ${grade} `)
+    return grade;    
+}
+
+studentGrades();
